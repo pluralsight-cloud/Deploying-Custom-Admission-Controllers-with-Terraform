623f46cea526a8cebff00b588faf77a5beaabb21
JesseHoch <jesse.hoch@linuxacademy.com>
2023-02-24 16:08:44 -0500

    Tweaked setup script

--- setup/homebrew_install.sh
+++ setup/homebrew_install.sh
@@ -2,21 +2,27 @@
 
 ### Installing Homebrew ###
 echo "Installing Homebrew"
-#yum -y install git docker
-sudo yum -y install git docker
-#usermod -a -G docker cloud_user
-sudo usermod -a -G docker cloud_user
-#su - cloud_user -c 'newgrp docker'
-newgrp docker
-#systemctl enable docker.service
-sudo systemctl enable docker.service
-#systemctl start docker.service
-sudo systemctl start docker.service
 
+### Install Docker ###
+yum -y install docker
+usermod -a -G docker cloud_user
+su - cloud_user -c 'newgrp docker'
+systemctl enable docker.service
+systemctl start docker.service
+
+sleep 3
+
+#yum -y install git
+sudo yum -y install git
+
+sleep 3
+
+### Installing Homebrew ###
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ### Adding Homebrew to your PATH ###
 echo "Adding Homebrew to your PATH"
+sleep 2
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/cloud_user/.bash_profile
 sleep 2
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
@@ -31,4 +37,4 @@ echo "Homebrew installation finished"
 
 ### Installing lab specific packages ###
 echo "Installing lab specific packages"
-brew install wget jq unzip gcc awscli terraform
+brew install wget jq unzip gcc awscli terraform kubectl
