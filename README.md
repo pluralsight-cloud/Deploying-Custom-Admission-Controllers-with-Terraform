949c1b6be31a7436973164b97e5f3325ce1e433f
JesseHoch <jesse.hoch@linuxacademy.com>
2023-02-24 10:46:36 -0500

    Added EKS Terraform configuration and README file.

---
+++ README.md
@@ -0,0 +1,4 @@
+Deploying Custom Admission Controllers with Terraform
+Jesse Hoch
+
+Course Repository for course #1265 "Deploying Custom Admission Controllers with Terraform"
---
+++ eks/main.tf
@@ -0,0 +1,21 @@
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
+
+provider "aws" {
+  access_key = var.AccessKey
+  secret_key = var.SecretKey
+  region = var.region
+}
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  cluster_name = "guru-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
