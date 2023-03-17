dc4a9a5e7fade477ed83c0711590a246c527827d
JesseHoch <jesse.hoch@linuxacademy.com>
2023-03-16 20:43:02 -0400

    Updated the HOL_2 config

--- HOL_2/mutating-webhook/generate-certs.sh
+++ HOL_2/mutating-webhook/generate-certs.sh
@@ -1,20 +1,20 @@
 # Certificate authority key and certificate
-openssl req -new -x509 -days 36500 -nodes -subj '/CN=Pod Label Add Webhook' -keyout ca.key -out ca.crt 
+openssl req -new -x509 -days 36500 -nodes -subj '/CN=Mutating Admission Webhook' -keyout ca.key -out ca.crt 
 
 # Server key
 openssl genrsa -out server.key 2048
 
 # Certificate signing request
-openssl req -new -key server.key -subj '/CN=pod-label-add.default.svc' -out server.csr
+openssl req -new -key server.key -subj '/CN=mutating-webhook.default.svc' -out server.csr
 
 # Server certificate
-openssl x509 -req -days 36500 -extfile <(printf "subjectAltName=DNS:pod-label-add.default.svc") -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt 
+openssl x509 -req -days 36500 -extfile <(printf "subjectAltName=DNS:mutating-webhook.default.svc") -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt 
 
 # Copy certs to app folder
 chmod 644 *.key
-cp ca.crt ./certs
-cp server.crt ./certs
-cp server.key ./certs
+cp ca.crt ./app
+cp server.crt ./app
+cp server.key ./app
 
 # Cleanup
 rm ca.crt
