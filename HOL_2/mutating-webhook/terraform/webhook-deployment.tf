da1382ec782d81b12495e70c84631d6f2e9fe0be
JesseHoch <jesse.hoch@linuxacademy.com>
2023-03-17 15:29:35 -0400

    Fixed cert issue

--- HOL_2/mutating-webhook/terraform/webhook-deployment.tf
+++ HOL_2/mutating-webhook/terraform/webhook-deployment.tf
@@ -78,3 +78,37 @@ resource "kubernetes_service" "mutating_webhook" {
   }
 }
 
+resource "kubernetes_mutating_webhook_configuration" "pod_label_add" {
+  depends_on = [kubernetes_deployment.mutating_webhook]
+  metadata {
+    name = "pod-label-add"
+
+    annotations = {
+      "cert-manager.io/inject-ca-from" = "default/client"
+    }
+  }
+
+  webhook {
+    name = "pod-label-add.guru.com"
+
+    client_config {
+      service {
+        namespace = "default"
+        name      = "mutating-webhook"
+        path      = "/mutate"
+      }
+    }
+
+    rule {
+      api_groups   = [""]
+      api_versions = ["v1"]
+      resources   = ["pods"]
+      operations  = ["CREATE"]
+      scope       = "Namespaced"
+    }
+
+    side_effects              = "None"
+    admission_review_versions = ["v1"]
+  }
+}
+
--- HOL_3/validating-webhook/terraform/webhook-deployment.tf
+++ HOL_3/validating-webhook/terraform/webhook-deployment.tf
@@ -66,3 +66,33 @@ resource "kubernetes_service" "validating_webhook" {
   }
 }
 
+resource "kubernetes_validating_webhook_configuration" "pod_label_require" {
+  depends_on = [kubernetes_deployment.validating_webhook]
+  metadata {
+    name = "pod-label-require"
+
+    annotations = {
+      "cert-manager.io/inject-ca-from" = "default/client"
+    }
+  }
+
+  webhook {
+    name = "pod-label-require.trstringer.com"
+
+    client_config {
+      service {
+        namespace = "default"
+        name      = "validating-webhook"
+        path      = "/validate"
+      }
+    }
+
+    rule {
+      operations = ["CREATE"]
+      scope      = "Namespaced"
+    }
+
+    side_effects              = "None"
+    admission_review_versions = ["v1"]
+  }
+}
